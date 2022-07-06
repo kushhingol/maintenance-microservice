270ce40262a5ba6e33417e92624074d7346d94de
Kush Hingol <kushhingol.kh@gmail.com>
2022-07-06 19:26:54 +0530

    updated sql query

--- src/maintenance/maintenance.sql.js
+++ src/maintenance/maintenance.sql.js
@@ -1,7 +1,7 @@
 const getSQLQueries = () => {
   return {
     addMaintenance: `INSERT INTO maintenance set ?`,
-    findAllMaintenanceData: `SELECT name, start_date, start_time, end_date, end_time from maintenance WHERE is_deleted = 0`,
+    findAllMaintenanceData: `SELECT name, start_date, start_time, end_date, end_time from maintenance WHERE is_deleted = 0 ORDER BY id desc`,
     deleteAllMaintenanceData: `UPDATE maintenance SET is_deleted = 1 WHERE is_deleted = 0`,
   };
 };
