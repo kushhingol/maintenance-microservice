845d7925856c523bc1562d2eae56b2d37d943a0a
Kush Hingol <kushhingol.kh@gmail.com>
2022-07-07 00:39:19 +0530

    updated setup_env variable to acoid exposing db creds

--- setup_env.sh
+++ setup_env.sh
@@ -6,11 +6,13 @@ fi
 
 touch $FILENAME
 
-echo DB_HOST=sql11.freemysqlhosting.net >> $FILENAME
-echo DB_DBNAME=sql11504331 >> $FILENAME
-echo DB_USER=sql11504331 >> $FILENAME
-echo DB_PASSWORD=UeGXmq83jn >> $FILENAME
+## need to use secret manager here so avoided adding env vars value
+## Do contact the author for env variable values
+echo DB_HOST= >> $FILENAME
+echo DB_DBNAME= >> $FILENAME
+echo DB_USER= >> $FILENAME
+echo DB_PASSWORD= >> $FILENAME
 echo TOKEN_EXPIRATION_TIME=10 >> $FILENAME
-echo SECRET_KEY=helloworld >> $FILENAME
+echo SECRET_KEY= >> $FILENAME
 echo NODE_ENV=production >> $FILENAME
 echo PORT=4000 >> $FILENAME
